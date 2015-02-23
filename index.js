b252d2574c160a2a2ceb4770c6a99500ee751463
Mikko Haapoja <me@mikkoh.com>
2015-02-23 14:37:08 -0500

    Implemented dispatching events for when people join and leave rooms

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var promise = require( 'bluebird' );
+var EventEmitter = require( 'events').EventEmitter;
 
 var curId = Number.MIN_VALUE, 
 	idToKey = {},
@@ -25,259 +26,273 @@ function storeMemory() {
 	this.generateKeys();
 }
 
-storeMemory.prototype = {
+var p = storeMemory.prototype = new EventEmitter();
 
-	/**
-	 * This will create a roomID, roomDataObject, and users array for the room
-	 * 
-	 * @return {Promise} This promise will return a room id once the room is created
-	 */
-	createRoom: function( userID ) {
 
-		var id = curId;
+/**
+ * This will create a roomID, roomDataObject, and users array for the room
+ * 
+ * @return {Promise} This promise will return a room id once the room is created
+ */
+p.createRoom = function( userID ) {
 
-		curId++;
+	var id = curId;
 
-		if( curId == Number.MAX_VALUE )
-			curId = Number.MIN_VALUE;
+	curId++;
 
-		roomUsers[ id ] = [];
+	if( curId == Number.MAX_VALUE )
+		curId = Number.MIN_VALUE;
 
-		return this.setRoomData( id, {} )
-			   .then( this.joinRoom.bind( this, userID, id ) )
-			   .then( function() {
+	roomUsers[ id ] = [];
 
-			   		return id;
-			   });
-	},
+	return this.setRoomData( id, {} )
+		   .then( this.joinRoom.bind( this, userID, id ) )
+		   .then( function() {
 
-	joinRoom: function( userID, roomID ) {
+		   		return id;
+		   });
+};
 
-		if( roomUsers[ roomID ] === undefined ) {
+p.joinRoom = function( userID, roomID ) {
 
-			return promise.reject( 'No room with the id: ' + roomID );
+	if( roomUsers[ roomID ] === undefined ) {
+
+		return promise.reject( 'No room with the id: ' + roomID );
+	} else {
+
+		if( roomUsers[ roomID ].indexOf( userID ) != -1 ) {
+
+			return promise.reject( 'User is already in room' );
 		} else {
 
-			if( roomUsers[ roomID ].indexOf( userID ) != -1 ) {
+			roomUsers[ roomID ].push( userID );
 
-				return promise.reject( 'User is already in room' );
-			} else {
+			this.emit( roomID, {
 
-				roomUsers[ roomID ].push( userID );
+				action: 'join',
+				user: userID,
+				users: roomUsers[ roomID ]
+			});
 
-				return promise.resolve( roomID );	
+			return promise.resolve( roomID );	
+		}
+	}
+};
+
+/**
+ * Remove a user from a room
+ * 
+ * @param  {String} userID an id for the user whose leaving
+ * @param  {String} roomID an id for the room the user is leaving
+ * @return {Promise} When this promise resolves it will send the number of users in the room
+ */
+p.leaveRoom = function( userID, roomID ) {
+
+	var userCount;
+
+	if( roomUsers[ roomID ] === undefined ) {
+
+		return promise.reject( 'No room with the id: ' + roomID );
+	} else {
+
+		var userIDX = roomUsers[ roomID ].indexOf( userID );
+
+		if( userIDX != -1 ) {
+
+			roomUsers[ roomID ].splice( userIDX, 1 );
+
+			userCount = roomUsers[ roomID ].length;
+
+			if( userCount == 0 ) {
+
+				delete roomUsers[ roomID ];
 			}
-		}
-	},
 
-	/**
-	 * Remove a user from a room
-	 * 
-	 * @param  {String} userID an id for the user whose leaving
-	 * @param  {String} roomID an id for the room the user is leaving
-	 * @return {Promise} When this promise resolves it will send the number of users in the room
-	 */
-	leaveRoom: function( userID, roomID ) {
+			this.emit( roomID, {
 
-		var userCount;
+				action: 'leave',
+				user: userID,
+				users: roomUsers[ roomID ]
+			});
 
-		if( roomUsers[ roomID ] === undefined ) {
-
-			return promise.reject( 'No room with the id: ' + roomID );
+			return promise.resolve( userCount );
 		} else {
 
-			var userIDX = roomUsers[ roomID ].indexOf( userID );
-
-			if( userIDX != -1 ) {
-
-				roomUsers[ roomID ].splice( userIDX, 1 );
-
-				userCount = roomUsers[ roomID ].length;
-
-				if( userCount == 0 ) {
-
-					delete roomUsers[ roomID ];
-				}
-
-				return promise.resolve( userCount );
-			} else {
-
-				return promise.reject( 'User ' + userID + ' is not in the room: ' + roomID );
-			}
+			return promise.reject( 'User ' + userID + ' is not in the room: ' + roomID );
 		}
-	},
+	}
+};
 
-	/**
-	 * get a key which can be used to enter a room vs entering room via
-	 * roomID
-	 * 
-	 * @param  {String} roomID id of the room you'd like a key for
-	 * @return {Promise} A promise will be returned which will return a roomKey on success
-	 */
-	getKey: function( roomID ) {
+/**
+ * get a key which can be used to enter a room vs entering room via
+ * roomID
+ * 
+ * @param  {String} roomID id of the room you'd like a key for
+ * @return {Promise} A promise will be returned which will return a roomKey on success
+ */
+p.getKey = function( roomID ) {
 
-		if( keys.length > 0 ) {
+	if( keys.length > 0 ) {
 
-			var randIdx = Math.round( Math.random() * ( keys.length - 1 ) ),
-				key = keys.splice( randIdx, 1 )[ 0 ];
+		var randIdx = Math.round( Math.random() * ( keys.length - 1 ) ),
+			key = keys.splice( randIdx, 1 )[ 0 ];
 
-			keyToId[ key ] = roomID;
-			idToKey[ roomID ] = key;
+		keyToId[ key ] = roomID;
+		idToKey[ roomID ] = key;
 
-			return promise.resolve( key );
+		return promise.resolve( key );
+	} else {
+
+		return promise.reject( 'Run out of keys' );
+	}
+};
+
+/**
+ * return a room key so someone else can use it.
+ * 
+ * @param  {String} roomID id of the room you'll be returning a key for
+ * @param  {String} key the key you'd like to return
+ * @return {Promise} This promise will succeed when the room key was returned
+ */
+p.returnKey = function( roomID, key ) {
+
+	return this.getRoomIdForKey( key )
+	.then( function( savedRoomId ) {
+
+		if( savedRoomId == roomID ) {
+
+			delete idToKey[ roomID ];
+			delete keyToId[ key ];
+
+			keys.push( key );
+
+			return promise.resolve();
 		} else {
 
-			return promise.reject( 'Run out of keys' );
+			return promise.reject( 'roomID and roomID for key do not match' );
 		}
-	},
+	});
+};
 
-	/**
-	 * return a room key so someone else can use it.
-	 * 
-	 * @param  {String} roomID id of the room you'll be returning a key for
-	 * @param  {String} key the key you'd like to return
-	 * @return {Promise} This promise will succeed when the room key was returned
-	 */
-	returnKey: function( roomID, key ) {
+/**
+ * return the room id for the given key
+ * 
+ * @param  {String} key key used to enter the room
+ * @return {Promise} This promise will succeed with the room id and fail if no room id exists for key
+ */
+p.getRoomIdForKey = function( key ) {
 
-		return this.getRoomIdForKey( key )
-		.then( function( savedRoomId ) {
+	var savedRoomId = keyToId[ key ];
 
-			if( savedRoomId == roomID ) {
+	if( savedRoomId ) {
 
-				delete idToKey[ roomID ];
-				delete keyToId[ key ];
+		return promise.resolve( savedRoomId );
+	} else {
 
-				keys.push( key );
+		return promise.reject();
+	}
+};
 
-				return promise.resolve();
-			} else {
+/**
+ * set a variable on the rooms data object
+ * 
+ * @param {String} roomID id for the room whose 
+ * @param {String} key variable name/key that you want to set
+ * @param {*} value Value you'd like to set for the variable
+ * @return {Promise} once this promise succeeds the rooms variable will be set
+ */
+p.setRoomDataVar = function( roomID, key, value ) {
 
-				return promise.reject( 'roomID and roomID for key do not match' );
-			}
-		});
-	},
+	if( roomData[ roomID ] === undefined )
+		roomData[ roomID ] = {};
 
-	/**
-	 * return the room id for the given key
-	 * 
-	 * @param  {String} key key used to enter the room
-	 * @return {Promise} This promise will succeed with the room id and fail if no room id exists for key
-	 */
-	getRoomIdForKey: function( key ) {
+	roomData[ roomID ][ key ] = value;
 
-		var savedRoomId = keyToId[ key ];
+	return promise.resolve( value );
+};
 
-		if( savedRoomId ) {
+/**
+ * get a variable from the rooms data object
+ * 
+ * @param {String} roomID id for the room
+ * @param {String} key variable name/key that you want to get
+ * @return {Promise} once this promise succeeds it will return the variable value it will fail if the variable does not exist
+ */
+p.getRoomDataVar = function( roomID, key ) {
 
-			return promise.resolve( savedRoomId );
-		} else {
+	if( roomData[ roomID ] === undefined ) {
 
-			return promise.reject();
-		}
-	},
+		return promise.reject( 'There is no room by that id' );
+	} else {
 
-	/**
-	 * set a variable on the rooms data object
-	 * 
-	 * @param {String} roomID id for the room whose 
-	 * @param {String} key variable name/key that you want to set
-	 * @param {*} value Value you'd like to set for the variable
-	 * @return {Promise} once this promise succeeds the rooms variable will be set
-	 */
-	setRoomDataVar: function( roomID, key, value ) {
+		return promise.resolve( roomData[ roomID ][ key ] );
+	}
+};
 
-		if( roomData[ roomID ] === undefined )
-			roomData[ roomID ] = {};
+/**
+ * delete a variable from the rooms data object
+ * 
+ * @param {String} roomID id for the room 
+ * @param {String} key variable name/key that you want to delete
+ * @return {Promise} once this promise succeeds it will return the value that was stored before delete
+ */
+p.delRoomDataVar = function( roomID, key ) {
 
-		roomData[ roomID ][ key ] = value;
+	var oldVal;
 
-		return promise.resolve( value );
-	},
+	if( roomData[ roomID ] === undefined ) {
 
-	/**
-	 * get a variable from the rooms data object
-	 * 
-	 * @param {String} roomID id for the room
-	 * @param {String} key variable name/key that you want to get
-	 * @return {Promise} once this promise succeeds it will return the variable value it will fail if the variable does not exist
-	 */
-	getRoomDataVar: function( roomID, key ) {
+		return promise.reject( 'There is no room by that id' );
+	} else {
 
-		if( roomData[ roomID ] === undefined ) {
+		oldVal = roomData[ roomID ][ key ];
 
-			return promise.reject( 'There is no room by that id' );
-		} else {
+		delete roomData[ roomID ][ key ];
 
-			return promise.resolve( roomData[ roomID ][ key ] );
-		}
-	},
+		return promise.resolve( oldVal );
+	}
+};
 
-	/**
-	 * delete a variable from the rooms data object
-	 * 
-	 * @param {String} roomID id for the room 
-	 * @param {String} key variable name/key that you want to delete
-	 * @return {Promise} once this promise succeeds it will return the value that was stored before delete
-	 */
-	delRoomDataVar: function( roomID, key ) {
+/**
+ * Receive the data stored for a room.
+ * 
+ * @param  {String} roomID id for the room you'd like data for
+ * @return {Promise} This promise will succeed when data is received for the room
+ */
+p.getRoomData = function( roomID ) {
 
-		var oldVal;
+	if( roomData[ roomID ] )
+		return promise.resolve( roomData[ roomID ] );
+	else
+		return promise.reject( 'No room data' );
+};
 
-		if( roomData[ roomID ] === undefined ) {
+/**
+ * Set data stored for a room.
+ * 
+ * @param  {String} roomID id for the room you'd like to set data for
+ * @return {Promise} This promise will succeed when data is set for the room
+ */
+p.setRoomData = function( roomID, data ) {
 
-			return promise.reject( 'There is no room by that id' );
-		} else {
+	roomData[ roomID ] = data;
 
-			oldVal = roomData[ roomID ][ key ];
+	return promise.resolve( data );
+};
 
-			delete roomData[ roomID ][ key ];
+/**
+ * generateKeys is a function which will create and store a set of 
+ * keys which can be used to enter a room instead of a room id
+ */
+p.generateKeys = function() {
 
-			return promise.resolve( oldVal );
-		}
-	},
+	if( !keys ) {
 
-	/**
-	 * Receive the data stored for a room.
-	 * 
-	 * @param  {String} roomID id for the room you'd like data for
-	 * @return {Promise} This promise will succeed when data is received for the room
-	 */
-	getRoomData: function( roomID ) {
+		keys = [];
 
-		if( roomData[ roomID ] )
-			return promise.resolve( roomData[ roomID ] );
-		else
-			return promise.reject( 'No room data' );
-	},
+		for( var i = 0; i < 9999; i++ ) {
 
-	/**
-	 * Set data stored for a room.
-	 * 
-	 * @param  {String} roomID id for the room you'd like to set data for
-	 * @return {Promise} This promise will succeed when data is set for the room
-	 */
-	setRoomData: function( roomID, data ) {
-
-		roomData[ roomID ] = data;
-
-		return promise.resolve( data );
-	},
-
-	/**
-	 * generateKeys is a function which will create and store a set of 
-	 * keys which can be used to enter a room instead of a room id
-	 */
-	generateKeys: function() {
-
-		if( !keys ) {
-
-			keys = [];
-
-			for( var i = 0; i < 9999; i++ ) {
-
-				keys.push( i );
-			}
+			keys.push( i );
 		}
 	}
 };
